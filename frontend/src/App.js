f4a694d2892e7cecb7e02c5cfe9d29bf9ee95d75
lim-youn-ho <limyounhoo@gmail.com>
2023-04-16 21:09:47 +0900

    [리액트] 상품등록 api호출 등록단 수정 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,6 @@ import Header from "./components/Header";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import AddHouses from "./components/AddHouses";
-import HousesBase from "./components/houses/HousesBase";
-import HousesDetail from "./components/houses/HousesDetail";
-import HousesRoom from "./components/houses/HousesRoom";
 
 import {BrowserRouter as Router,
     Routes,
@@ -22,9 +19,6 @@ function App() {
                 <Route path="/houses" element={<AddHouses />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/housesBase" element={<HousesBase />} />
-                <Route path="/housesDetail" element={<HousesDetail />} />
-                <Route path="/housesRoom" element={<HousesRoom />} />
             </Routes>
         </Router>
     );
--- frontend/src/components/AddHouses.js
+++ frontend/src/components/AddHouses.js
@@ -1,60 +1,111 @@
-import React, { Component } from "react";
-import { Link  } from "react-router-dom";
-import {Menu} from 'semantic-ui-react'
-import HousesBase from "./houses/HousesBase";
-
-
-const colors = [
-    'black',
-]
+import React, { Component,useState } from "react";
+import {Button, Form, Menu} from 'semantic-ui-react'
 
 
 
-class ExampleMenu extends Component {
-    state = { activeItem: 'home' }
-
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-    handleClick = () => {
-        // 클릭 이벤트 처리
-    };
-    render() {
-        const { color } = this.props
-        const { activeItem } = this.state
 
 
-        return (
-            <Menu color={color} widths={3} converted style={{fontFamily: 'TheJamsil5Bold'}}>
-                <Menu.Item
-                   /* name='기본정보'
-                    active={activeItem === 'home'}
-                    onClick={this.handleClick}*/
-                >
-                    <Link to="/housesBase" onClick={this.handleClick}>기본정보</Link>
-                </Menu.Item>
-                <Menu.Item
-                    name='상세정보'
-                    active={activeItem === 'messages'}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name='방정보'
-                    active={activeItem === 'friends'}
-                    onClick={this.handleItemClick}
-                />
-            </Menu>
-        );
-    }
-}
+
 const AddHouses = () => {
-    
-    const menus = colors.map((color) => <ExampleMenu color={color} key={color} />)
+
+    const [menu, setMenu] = useState("base");
+    const [name, setName] = useState("");
+    const [acceptedGender, setAcceptedGender] = useState("");
+    const [address, setAddress] = useState("");
+   const handleClick = (menuName) => {
+        setMenu(menuName);
+    };
+    const buttonClick = (gender) => {
+        setAcceptedGender(gender);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('/houses', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json;charset=utf-8' },
+            body: JSON.stringify({
+                name:name,
+                acceptedGender:acceptedGender,
+                address:address,
+            }),
+        })
+            .then(res => res.json())
+            .then(data => console.log(data));
+    };
 
     return(
-        <div>{menus}</div>
+        <div>
+                <Menu widths={3} converted style={{fontFamily: 'TheJamsil5Bold'}}>
+                    <Menu.Item
+                        name='기본정보'
+                        onClick={() =>  handleClick("base")}
+                    />
+
+                    <Menu.Item name='상세정보'
+                               onClick={() =>  handleClick("detail")}
+                    />
+                    <Menu.Item name='방정보'
+                               onClick={() =>  handleClick("rooms")}
+                    />
+                </Menu>
+            {/*기본정보*/}
+                {menu === "base" && (
+
+                    <Form >
+                        <Form.Field>
+                            <label htmlFor="name">하우스이름</label>
+                            <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                        </Form.Field>
+                        <Form.Field>
+                            <label htmlFor="name">수용 성별</label>
+                            <Button
+                                label='남성'
+                                value='male'
+                                onClick={(e, data) => buttonClick(data.value)}
+                            />
+                            <Button
+                                label='여성'
+                                value='female'
+                                onClick={(e, data) => buttonClick(data.value)}
+                            />
+                            <Button
+                                label='혼성'
+                                value='all'
+                                onClick={(e, data) => buttonClick(data.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <label htmlFor="address">주소</label>
+                            <input type="text" id="address" value={address} onChange={(e) => setAddress(e.target.value)} />
+                        </Form.Field>
+                        <Button onClick={() =>  handleClick("detail")}>다음</Button>
+                    </Form>
+                )}
+            {/*상세정보*/}
+                {menu === "detail" && (
+                    <Form >
+                        <Form.Field>
+                            <label htmlFor="name">상세 정보</label>
+                        </Form.Field>
+                            <Button onClick={() =>  handleClick("base")}>이전</Button>
+                            <Button onClick={() =>  handleClick("rooms")}>다음</Button>
+                    </Form>
+                )}
+            {/*방정보*/}
+                {menu === "rooms" && (
+                    <Form onSubmit={handleSubmit} >
+                        <Form.Field>
+                            <label htmlFor="name">방 정보</label>
+                        </Form.Field>
+                            <Button onClick={() =>  handleClick("detail")}>이전</Button>
+                            <Button color = "blue" type="submit">등록하기</Button>
+                    </Form>
+                )}
+
+        </div>
+
     )
-    /*<p>
-        이미 계정이 있으신가요? <Link to="/login">로그인</Link>
-    </p>*/
 
 };
 
